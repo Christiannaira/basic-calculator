cfe7626cc7a0dceadb6e25238caf6de48cea4600
Christian <Christianmnaira@gmail.com>
2024-09-02 10:44:39 +0800

    feat: Added a basic functionality for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 
+
+function CalcInput({inputValue}) {
+
+  const handleCLick = () => {
+
+      alert(inputValue + 2);
+
+  }
+
+  return (
+
+    <div onClick={handleCLick} className="calculator_value_input">
+
+      {inputValue}
+
+    </div>
+
+  )
+
+}
 
 function App() {
+
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [secondNumber, setSecondNumber] = useState(null);
+  const [operator, setOperator] = useState(null);
 
 
 
@@ -9,15 +34,21 @@ function App() {
     
       <div className="calculator_container">
 
-        <div>9</div>
-        <div>8</div>
-        <div>7</div>
-        <div>6</div>
-        <div>5</div>
-        <div>4</div>
-        <div>3</div>
-        <div>2</div>
-        <div>1</div>
+        <div className="result_container">
+
+        
+
+        </div>
+
+        <CalcInput inputValue={1}/>
+        <CalcInput inputValue={2}/>
+        <CalcInput inputValue={3}/>
+        <CalcInput inputValue={4}/>
+        <CalcInput inputValue={5}/>
+        <CalcInput inputValue={6}/>
+        <CalcInput inputValue={7}/>
+        <CalcInput inputValue={8}/>
+        <CalcInput inputValue={9}/>
 
       </div>
 
